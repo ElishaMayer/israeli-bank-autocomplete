54ebaad75d86f3a542eec6b83579bffd2ba564d5
Elisha Mayer <elisja.mayer@gmail.com>
2024-02-06 22:53:56 +0200

    use data.gov.il as data source

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,39 +1,50 @@
-import {fetchNewDataFromIsraelBank, getAutocompleteSuggestions} from "../index";
+import BankDataSource from "../index";
+import data from './bank-data.json';
+
+const bankDataSource = new BankDataSource(data);
 
 test("test with no options", () => {
-    expect(getAutocompleteSuggestions("620")[1].branchCode).toStrictEqual("620");
+    expect(bankDataSource.getAutocompleteSuggestions("1")[1].branchCode).toStrictEqual(1);
 });
 
 test("test with bankCode option", () => {
     expect(
-        getAutocompleteSuggestions("620", {bankCode: 12})[0].branchCode
-    ).toStrictEqual("620");
+        bankDataSource.getAutocompleteSuggestions("1", { bankCode: 18 })[0].branchCode
+    ).toStrictEqual(1);
 });
 
 test("test with bankCode option", () => {
     expect(
-        getAutocompleteSuggestions("620", {bankCode: 12}).length
+        bankDataSource.getAutocompleteSuggestions("1", { bankCode: 18 }).length
     ).toStrictEqual(1);
 });
 
 test("test with inputType=BRANCH_NAME option", () => {
     expect(
-        getAutocompleteSuggestions("620", {inputType: "BRANCH_NAME"}).length
+        bankDataSource.getAutocompleteSuggestions("1", { inputType: "BRANCH_NAME" }).length
     ).toStrictEqual(0);
 });
 
 test("test with inputType=BRANCH_CODE option", () => {
     expect(
-        getAutocompleteSuggestions("620", {inputType: "BRANCH_CODE"}).length
-    ).toStrictEqual(3);
+        bankDataSource.getAutocompleteSuggestions("1", { inputType: "BRANCH_CODE" }).length
+    ).toStrictEqual(2);
 });
 
 test("test with inputType=BRANCH_CODE && bankCode option", () => {
     expect(
-        getAutocompleteSuggestions("620", {inputType: "BRANCH_CODE", bankCode: 12}).length
+        bankDataSource.getAutocompleteSuggestions("1", { inputType: "BRANCH_CODE", bankCode: 22 }).length
     ).toStrictEqual(1);
 });
 
-test("test fetch new data from Israel bank", async () => {
-    await fetchNewDataFromIsraelBank()
+test("test fetch new data from data gov into file", async () => {
+    await bankDataSource.fetchNewDataFromDataGovIntoFile('2202bada-4baf-45f5-aa61-8c5bad9646d3', 10);
+    const fs = require('fs');
+    expect(fs.existsSync('bank-data.json')).toBeTruthy();
+});
+
+test("test fetch new data from data gov", async () => {
+    await bankDataSource.fetchNewDataFromDataGov('2202bada-4baf-45f5-aa61-8c5bad9646d3', 10);
+    expect(bankDataSource.branches.length).toBeGreaterThan(9);
+    expect(bankDataSource.banks.length).toBeGreaterThan(3);
 });
