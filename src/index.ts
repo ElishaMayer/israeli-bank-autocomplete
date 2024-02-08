9a8d8740d93b9d6a51ef67f1a32f66e7a667ef68
Elisha Mayer <elisja.mayer@gmail.com>
2024-02-08 09:10:45 +0200

    BREAKING CHANGE: remove deprecated global functions

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
 import BankDataSource from "./BankDataSource";
-import bankData from './bank-data.json';
-
-const bankDataSource = new BankDataSource(bankData);
-
-/**
- * @deprecated
- */
-export const getAutocompleteSuggestions = bankDataSource.getAutocompleteSuggestions.bind(bankDataSource);
-
-/**
- * @deprecated
- */
-export const getAllBranches = bankDataSource.getAllBranches.bind(bankDataSource);
-
-/**
- * @deprecated
- */
-export const getAllBanks = bankDataSource.getAllBanks.bind(bankDataSource);
 
 export default BankDataSource;
